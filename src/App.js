b6735c913780a526af3c884721ee77edb4357262
Rithvik Kasarla <rithvikrk719@gmail.com>
2022-05-31 18:49:58 -0400

    progresslog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Sortervisuals from './components/Sortervisuals/Sortervisuals'
+import Headervisuals from './components/Headervisuals/Headervisuals'
+
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      {/* Component  which holds bars and updates positions as they get sorted*/}
+      <Sortervisuals></Sortervisuals>
     </div>
   );
 }
 
-export default App;
+export default App
